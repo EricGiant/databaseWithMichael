84ad97d81a079b0ba47c15cafebc00e5b3697319
EricGiant <ericspier@hotmail.com>
2022-03-01 11:22:39 +0100

    Added webpage file + css file

---
+++ webpage/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Webpage</title>
+        <link rel = "stylesheet" href = "index.css">
+    </head>
+    <body>
+        TEST
+    </body>
+</html>
