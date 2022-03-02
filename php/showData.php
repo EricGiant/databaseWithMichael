1605691919f980706212f3cdec2bd877d3ff4119
EricGiant <ericspier@hotmail.com>
2022-03-02 12:39:16 +0100

    database  + agecalc

---
+++ php/showData.php
@@ -0,0 +1,13 @@
+<?php
+//collect info from server
+
+//test info
+$voorNaam = "Eric";
+$achterNaam = "Spier";
+$geboorteDatum = "2005-07-11";
+echo("<div id = 'selectedDataGrid'>");
+echo("<div>" . $voorNaam . "</div>");
+echo("<div>" . $achterNaam . "</div>");
+echo("<div>" . $geboorteDatum . "</div>");
+echo("</div>");
+?>
--- webpage/index.php
+++ webpage/index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Webpage</title>
         <link rel = "stylesheet" href = "index.css">
+        <link rel = "stylesheet" href = "showData.css">
     </head>
     <body>
         TEST
