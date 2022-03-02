dd632d28ff6f1cc9f7a5895e81366c6c38430a73
Michael090103 <michael.schuiling97@gmail.com>
2022-03-02 12:53:32 +0100

    test

--- webpage/index.php
+++ webpage/index.php
@@ -1,11 +1,18 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html lang="en">
     <head>
+        <meta charset="UTF-8">
         <title>Webpage</title>
-        <link rel = "stylesheet" href = "index.css">
-        <link rel = "stylesheet" href = "showData.css">
+        <link rel="stylesheet" href="index.css">
+        <link rel="stylesheet" href="showData.css">
+        <style>
+            body {
+                background-color: rgb(33, 33, 33);
+                color: white;
+            }
+        </style>
     </head>
     <body>
-        TEST
+
     </body>
 </html>
