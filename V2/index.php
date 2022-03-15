9e12d4672af2b985480cda2d0a08aeaee904adbb
Michael090103 <michael.schuiling97@gmail.com>
2022-03-15 09:15:36 +0100

    Make basic index file

--- V2/index.php
+++ V2/index.php
@@ -7,6 +7,17 @@
     <title>Home</title>
 </head>
 <body>
-    
+<a href="selectInfo.php">shidfard</a>
+<form method="post">
+    <label for="firstName">Voornaam:<br/>
+        <input type="text" name="firstName">
+    </label>
+    <label for="lastName">Achternaam:<br/>
+        <input type="text" name="lastName">
+    </label>
+    <label for="birthDate">Geboortedatum:<br/>
+        <input type="date" name="lastName">
+    </label>
+</form>
 </body>
 </html>
