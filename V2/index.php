3863f5ed6bc1ad1b3a747babc310f87d974f73ca
Michael090103 <michael.schuiling97@gmail.com>
2022-03-15 09:51:34 +0100

    insert database entries from form

--- V2/index.php
+++ V2/index.php
@@ -6,6 +6,28 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="index.css">
     <title>Home</title>
+    <?php
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "b2v2";
+
+    if (!empty($_POST['firstName']) && !empty($_POST['lastName']) && !empty($_POST['birthdate'])) {
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $birthdate = $_POST['birthdate'];
+
+        $conn = new mysqli($server, $username, $password, $database);
+
+        $sql = "INSERT INTO `userdata` (`ID`, `firstName`, `lastName`, `birthdate`) VALUES (NULL, '$firstName', '$lastName', '$birthdate')";
+        if (mysqli_query($conn, $sql)) {
+            echo "Succes";
+        } else {
+            echo "<b>ERROR: </b>" . $sql . "<br/>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+    }
+    ?>
 </head>
 <body>
 <a href="selectInfo.php">shidfard</a>
